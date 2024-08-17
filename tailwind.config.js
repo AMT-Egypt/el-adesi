9962de10bac20fcf2cd17663d54bba3be9886e18
ahmedmansour779 <ahmedmansour1239@gmail.com>
2024-08-17 23:56:08 +0300

    add two fills and tailwand css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#593622",
+        secondary: "#F2EFC2",
+        third: "#A4A67C",
+        fourth: "#717340",
+        fives: "#F2F2F2"
+      }
+    },
+  },
+  plugins: [],
+}
+
