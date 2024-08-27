5694e68e4edadc1cc433b852f76e2838086a0dae
Mostafa Abdelwahed <65525706+Mostafa4Abdelwahed@users.noreply.github.com>
2024-08-27 13:40:47 -0700

    Delete Api Not working & Add ServiceDataType

--- src/functions/apis/getServices.ts
+++ src/functions/apis/getServices.ts
@@ -1,14 +1,13 @@
 import React, { SetStateAction } from "react";
 import { marriageDataType } from "../../types/typesData";
 
-export const fetchMarriageData = async (setData: React.Dispatch<SetStateAction<marriageDataType[]>>) => {
+export const GetServiceData = async (setData: React.Dispatch<SetStateAction<marriageDataType[]>>) => {
     try {
-        const response = await fetch('https://json-server-jade-two.vercel.app/marriage');
+        const response = await fetch(import.meta.env.SERVER_URL);
 
         if (!response.ok) {
             throw new Error(`Error: ${response.status}`);
         }
-
         const data: marriageDataType[] = await response.json();
         setData(data);  // You can handle the data here
     } catch (error) {
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
+import NavBarService from "../components/NavBarService";
+import Carts from "../modules/details/Carts";
+import { GetServiceData } from "../functions/apis/getServices";
+import { illnessDataType } from "../types/typesData";
 
 export default function Services() {
+  const [data, setData] = useState<illnessDataType>([])
+  const setFilter = ()=>{}
+  const setSearch = ()=>{}
+  useEffect(()=>{
+    GetServiceData(setData)
+  })
   return (
     <div className="flex justify-center min-h-[81.5vh] py-6 bg-secondary">
       <div className="container flex flex-col gap-8">
         <h1 className="text-2xl font-bold text-center text-primary">الخدمات</h1>
-        {/* <NavBarService setFilter={setFilter} setSearch={setSearch} />
-        <Carts/> */}
+        <NavBarService setFilter={setFilter} setSearch={setSearch} />
+        <Carts allData={data} />
         Test
       </div>
     </div>
